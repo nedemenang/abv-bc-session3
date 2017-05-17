d7c8d8efed261df5bc34daa8a9855ecfb1199196
ABS User <ABS User>
2017-05-17 21:48:42 +0100

    Write find missing function, pass all test

--- app/library.js
+++ app/library.js
@@ -21,5 +21,24 @@ reverseString: (str) =>
 	    else{
 	      return newString;
 	    }
-	}	
+	},
+
+	findMissing: (arrayA, arrayB) =>
+	{
+
+		   var arrayBSize = arrayB.length;
+
+		   // loop through previous array
+		   for(var j = 0; j < arrayBSize; j++) {
+
+		      // look for same thing in new array
+		      if (arrayA.indexOf(arrayB[j]) == -1)
+		         return arrayB[j];
+
+   			}
+
+   		return 0;
+
+	}
+	
 }
